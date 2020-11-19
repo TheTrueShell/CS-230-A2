6fa190e84679b153f12fab74c2d73cbaca5c712e
Aodan and David <1900529@swansea.ac.uk>
2020-11-19 17:16:31 +0000

    Created BackTrackTiles and DoubleMoveTiles

---
+++ DoubleMoveTile.java
@@ -0,0 +1,12 @@
+/**
+ * DoubleMoveTile.java
+ * @author William Aodan Telford and Deividas Prokopovicius
+ * @version 0.1.0
+ *  - no copyright
+ *  This class implements the DoubleMoveTile. This allows the player to perform a second action.
+ */
+
+public class DoubleMoveTile {
+
+
+}
