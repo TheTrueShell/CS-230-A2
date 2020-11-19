eef196edfff2f78995c642fbb30ed49baca89b91
Rory Durrant <1906816@swansea.ac.uk>
2020-11-19 18:19:11 +0000

    Added getters and setters for position

--- Player.java
+++ Player.java
@@ -9,21 +9,39 @@ public class Player {
 
     /** Represents current position with X and Y
      */
-    private int[][] CurrentPosition;
-    /** Contains a list of previous positions */
-    private int[][] PreviousPositions;
+    private int[][] CurrentPosition = new int[1][1];
+    /** Contains x and y of the previous position */
+    private int[] PreviousPosition = new int[2];
+    /** Contains x and y of the second previous position */
+    private int[] PreviousPosition2 = new int[2];
     /** Contains a hand of cards */
     private ArrayList<Tile> HandOfCards;
     /** Contains the current profile */
     private Profile currentProfile;
+    /** Stores the current name */
+    private String name;
 
     public Player(int x, int y, String name){
-
+        CurrentPosition[0][0] = x;
+        this.name = name;
     }
 
-    public int[][] getPreviousPositions() {
-        return PreviousPositions;
+    public int[] getPreviousPosition() {
+        return PreviousPosition;
     }
 
+    public int[] getPreviousPosition2(){
+        return PreviousPosition2;
+    }
+
+    public void setPreviousPosition(int x, int y) {
+        PreviousPosition[0] = x;
+        PreviousPosition[1] = y;
+    }
+
+    public void setPreviousPosition2(int x, int y) {
+        PreviousPosition2[0] = x;
+        PreviousPosition2[1] = y;
+    }
 
 }
