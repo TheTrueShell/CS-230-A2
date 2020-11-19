5301a3d9f93a099fd71455d95fcbd41af427e7fc
Aodan and David <1900529@swansea.ac.uk>
2020-11-19 15:59:33 +0000

    Added JavaDoc and started basic implementation of FloorTile. - use this david

--- FloorTile.java
+++ FloorTile.java
@@ -1,9 +1,46 @@
-public abstract class FloorTile {
+/**
+ * FloorTile.java
+ * @author William Aodan Telford and Deividas Prokopovicius
+ * - no copyright
+ *  This class implements the FloorTile superclass and some basic methods.
+ */
+
+public abstract class FloorTile extends Tile {
 
     private boolean isTileFixed;
     private boolean isFrozen;
     private boolean isOnFire;
     private int tileRotation; //In 360 degrees
     private boolean[] acessibleSides = new boolean[4]; // Top. right, bottom, Left
+    public abstract void play();
+
+    public FloorTile(int tileRotation) throws Exception {
+
+        try {
+        this.setRotation(tileRotation);
+        } catch (Exception ex) {
+
+            throw new Exception(ex.getMessage());
+
+        } 
+
+    }
+
+    public void setRotation(int rotation) throws Exception {
+
+        if (rotation % 90 != 0) {
+
+            throw new Exception("Cannot Rotate tile outside of 90 degree increments");
+
+        } else if (rotation > 360 || rotation < 0) {
+
+            throw new Exception("Cannot set rotate above 360 degrees or less than 0 degrees");
+
+        } else {
+
+            this.tileRotation = rotation;
+
+        }
+    }
 
 }
