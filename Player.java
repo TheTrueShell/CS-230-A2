2474e6de3dc81e6e97604869b6580f0bf0e76ecf
Rory Durrant <1906816@swansea.ac.uk>
2020-11-20 17:52:49 +0000

    Added new functions.

--- Player.java
+++ Player.java
@@ -10,34 +10,57 @@ import java.util.ArrayList;
 public class Player {
 
 
-    /** Represents current position with X and Y
+    /**
+     * Represents current position with X and Y
      */
-    private int[][] CurrentPosition = new int[1][1];
-    /** Contains x and y of the previous position */
+    private int[] CurrentPosition = new int[2];
+    /**
+     * Contains x and y of the previous position
+     */
     private int[] PreviousPosition = new int[2];
-    /** Contains x and y of the second previous position */
+    /**
+     * Contains x and y of the second previous position
+     */
     private int[] PreviousPosition2 = new int[2];
-    /** Contains a hand of cards */
+    /**
+     * Contains a hand of cards
+     */
     private ArrayList<Tile> HandOfCards;
-    /** Contains the current profile */
+    /**
+     * Contains the current profile
+     */
     private Profile currentProfile;
     /** Stores the current name */
     //private String name;
 
     /**
-     *
-     * @param x - Represents the X coordinate of the player.
-     * @param y - Represents the Y coordinate of the player
+     * @param x    - Represents the X coordinate of the player.
+     * @param y    - Represents the Y coordinate of the player
      * @param name - Represents the name of the player.
      */
     public Player(int x, int y, String name) {
-        CurrentPosition[0][0] = x;
+        CurrentPosition[0] = x;
+        CurrentPosition[1] = y;
         findCurrentProfile(name);
     }
 
     private Profile findCurrentProfile(String name) {
         //TODO
         return null;
+    }
+
+    /**
+     * @return - Returns X position of player.
+     */
+    public int getX(){
+        return CurrentPosition[0];
+    }
+
+    /**
+     * @return - Returns Y position of player.
+     */
+    public int getY(){
+        return CurrentPosition[1];
     }
 
     /**
@@ -56,6 +79,7 @@ public class Player {
 
     /**
      * Sets the position the player was last in.
+     *
      * @param x - Represents the X coordinate of the player.
      * @param y - Represents the Y coordinate of the player
      */
@@ -66,12 +90,24 @@ public class Player {
 
     /**
      * Sets the position the player was in 2 turns ago.
+     *
      * @param x Represents the X coordinate of the player.
      * @param y - Represents the Y coordinate of the player
      */
     public void setPreviousPosition2(int x, int y) {
         PreviousPosition2[0] = x;
         PreviousPosition2[1] = y;
+    }
+
+    /**
+     * @return - Returns current profile associated with this player.
+     */
+    public Profile getProfile() {
+        return currentProfile;
+    }
+
+    public int numOfTiles(){
+        return HandOfCards.size();
     }
 
     /**
