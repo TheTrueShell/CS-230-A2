f6b1875bf6ace4c476131b6666790893ad63f34b
JoelLaw01 <72454886+JoelLaw01@users.noreply.github.com>
2020-11-20 11:40:37 +0000

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -1,20 +1,29 @@
 /**
- * This class controls the basiscs of the individual player profiles.
+ * This class controls the basics of the individual player profiles.
  * @author Rory Durrant, Joel Lawless
  * @version 0.0.1
  */
 public class Profile {
 
     private String Name;
+
     private int GamesPlayed;
+
     private int GamesWon;
+
     private int GamesLost;
 
-    /** */
+    /**
+     * Create a new profile with the name given as parameter.
+     * @param name the name of the profile
+     *  */
     public Profile(String name){
         this.Name = name;
     }
 
+    /**
+     * returns each of the variables.
+     */
     public int getGamesPlayed() {
         return GamesPlayed;
     }
@@ -30,4 +39,15 @@ public class Profile {
     public String getName() {
         return Name;
     }
+
+    /**
+     * Increments each of the variables by 1.
+     */
+    public void updateGamesPlayed() {GamesPlayed++ ;}
+
+    public void updateGamesLost() {GamesLost++ ;}
+
+    public void updateGamesWon() {GamesWon++ ;}
+
+
 }
