c89f9aae21a26c741cd5a35fdc9e87755887aa18
Ben Rockley <whizzywig.rockley@gmail.com>
2020-11-27 18:31:15 +0000

    Add test generation

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -1,2 +1,11 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class BoardGUI {
+    @FXML
+    private Label header;
+    @FXML
+    public void initialize(){
+        header.setText("Text generated at runtime");
+    }
 }
