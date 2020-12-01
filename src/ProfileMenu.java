93f0207d95a3695bf335f9f0e0b24f3482b59f74
Ben Rockley <whizzywig.rockley@gmail.com>
2020-12-01 19:10:14 +0000

    Implemented back button

--- src/ProfileMenu.java
+++ src/ProfileMenu.java
@@ -7,12 +7,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ProfileMenu {
     private Game game;
 
     @FXML
     public void initialize(){
         //get profiles
+    }
+
+    @FXML
+    public void backButtonAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenuGUI.fxml"));
+        Parent mainMenuFXMLParent = (Parent)loader.load();
+        Scene mainMenuFXMLScene = new Scene(mainMenuFXMLParent);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // This line gets the stage the 'Play' button's action event came from
+        primaryStage.setScene(mainMenuFXMLScene);
+        MainMenuGUI controller = (MainMenuGUI)loader.getController();
+        controller.setGame(this.game);
+        primaryStage.show();
     }
 
     public void setGame(Game game){
