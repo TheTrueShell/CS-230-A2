b93f10ec37a76b49c60ea9b9e9da6b477270626d
Aodan and David <1900529@swansea.ac.uk>
2020-12-01 17:44:30 +0000

    Play button connected to GameMenu. Quit botton fixed

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -21,11 +21,15 @@ public class MainMenuGUI {
      * @throws IOException if the .fxml file cannot be found
      */
     public void playButtonAction(ActionEvent actionEvent) throws IOException {
-        Parent boardGUIParent = FXMLLoader.load(getClass().getResource("BoardGUI.fxml"));
+        Parent boardGUIParent = FXMLLoader.load(getClass().getResource("GameMenu.fxml"));
         Scene boardGUIScene = new Scene(boardGUIParent);
         Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         // This line gets the stage the 'Play' button's action event came from
         primaryStage.setScene(boardGUIScene);
         primaryStage.show();
+    }
+
+    public void quitButtonAction(ActionEvent actionEvent) throws IOException {
+        System.exit(0);
     }
 }
