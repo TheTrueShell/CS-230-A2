89cf378ad372f69a638e90456fd6724919743887
Gus Rendle <gus.rendle@Protonmail.com>
2020-12-01 17:15:32 +0000

    Fixed the scene switching.

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -1,17 +1,31 @@
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The Main Menu Scene's controller.
+ * @author Gus Rendle
+ * @version 1.1
+ */
 public class MainMenuGUI {
-    @FXML public BorderPane mainMenuPanel;
 
-    @FXML
+    /**
+     * The method for when the 'Play' button is clicked.
+     * It switches to the BoardGUI Scene
+     * @param actionEvent  the event of the 'Play' button being pressed
+     * @throws IOException if the .fxml file cannot be found
+     */
     public void playButtonAction(ActionEvent actionEvent) throws IOException {
-        BorderPane BoardPanel = FXMLLoader.load(getClass().getResource("BoardGUI.fxml"));
-        mainMenuPanel.getChildren().setAll(BoardPanel);
+        Parent boardGUIParent = FXMLLoader.load(getClass().getResource("BoardGUI.fxml"));
+        Scene boardGUIScene = new Scene(boardGUIParent);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // This line gets the stage the 'Play' button's action event came from
+        primaryStage.setScene(boardGUIScene);
+        primaryStage.show();
     }
 }
