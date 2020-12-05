0c6dba0e6d20ad6f4e5fd87b9f77ab2cc156301c
Aodan and David <1900529@swansea.ac.uk>
2020-12-05 18:58:11 +0000

    Added back button to loadGame

--- src/LoadGameGUI.java
+++ src/LoadGameGUI.java
@@ -1,3 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoadGameGUI {
 
     private Game game;
@@ -5,4 +15,19 @@ public class LoadGameGUI {
     public void setGame(Game game){
         this.game = game;
     }
+
+    @FXML
+    public void backButtonAction(ActionEvent actionEvent) throws IOException {
+        Game.playMenuSound();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenuGUI.fxml"));
+        Parent mainMenuFXMLParent = (Parent)loader.load();
+        Scene mainMenuFXMLScene = new Scene(mainMenuFXMLParent);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // This line gets the stage the 'Play' button's action event came from
+        primaryStage.setScene(mainMenuFXMLScene);
+        MainMenuGUI controller = (MainMenuGUI)loader.getController();
+        controller.setGame(this.game);
+        primaryStage.show();
+    }
+
 }
