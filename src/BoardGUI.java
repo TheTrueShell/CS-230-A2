8d2f9ad7c9527bb5f303a34efa1e54dcf65dc573
Ben Rockley <whizzywig.rockley@gmail.com>
2020-12-07 02:30:51 +0000

    Removed bad try catch's

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -402,18 +402,22 @@ public class BoardGUI {
         ActionTile t =
                 (ActionTile) this.game.getTurn().getHand().get(this.handIndex);
         //play selected tile at mouseX-1 and mouseY-1
-        try {
+        if (t instanceof ActionTileFloor){
             ActionTileFloor tile = (ActionTileFloor) t;
-            tile.action(this.game.getBoard(), (int) mouseX, (int) mouseY,
-                    this.game.getPlayers().size());
-        } catch (Exception e) {
-            //not a action floor tile
-            if (t instanceof DoubleMoveTile) {
-                this.doubleMove = true;
-            } else {
-                this.backTrack = true;
+            try {
+                tile.action(this.game.getBoard(), (int) mouseX, (int) mouseY,
+                        this.game.getPlayers().size());
+            } catch (Exception e){
+                //if null values passed to the action
+                e.printStackTrace();
             }
+        } else if (t instanceof DoubleMoveTile) {
+            //not a action floor tile
+            this.doubleMove = true;
+        } else {
+            this.backTrack = true;
         }
+
         game.playActionSound(t.getTILETYPE());
         drawCanvas();
         isAbleToMove(this.game.getTurn());
@@ -444,12 +448,12 @@ public class BoardGUI {
         Tile drawnTile = game.getBag().getRandomTile();
         p.addToHand(drawnTile);
         drawCanvas();
-        try {
+        if (drawnTile instanceof ActionTile){
             ActionTile actionTile = (ActionTile) drawnTile;
             //choose to play it
             playerTurnTag.setText("Do you want to play the action Tile");
             turnProgression = 1;
-        } catch (Exception e) {
+        } else if (drawnTile instanceof FloorTile) {
             FloorTile floorTile = (FloorTile) drawnTile;
             //must play this now
             if (canPlaceTile()) {
